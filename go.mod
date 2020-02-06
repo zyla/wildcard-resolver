cf98b27ac8340a548d9177cfaff3e590352bc702
Maciej Bielecki <zyla@prati.pl>
2020-02-06 13:34:38 +0100

    Add config

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/docker/libnetwork v0.5.6
 	github.com/miekg/dns v1.1.27
+	github.com/ryanuber/go-glob v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
 )
--- main.go
+++ main.go
@@ -4,17 +4,36 @@ import (
 	"fmt"
 	"net"
 	"context"
+    "os"
+    "strings"
 
 	"github.com/miekg/dns"
+    "github.com/ryanuber/go-glob"
 )
 
-// resolver implements the Resolver interface
 type resolver struct {
+    mappings []mapping
+}
+
+type mapping struct {
+    pattern string
+    target string
 }
 
 func main() {
-
 	var r resolver
+
+    config := os.Getenv("HOSTS")
+    if len(config) > 0 {
+        pairs := strings.Split(config, ",")
+        for _, pair := range pairs {
+            split := strings.Split(pair, ":")
+            r.mappings = append(r.mappings, mapping{
+                pattern: split[0],
+                target: split[1],
+            })
+        }
+    }
 
 	addr := &net.UDPAddr{
 		IP:   net.ParseIP("0.0.0.0"),
@@ -37,17 +56,27 @@ func (r *resolver) ServeDNS(w dns.ResponseWriter, query *dns.Msg) {
 		return
 	}
 
-	name := query.Question[0].Name
+	origname := query.Question[0].Name
+    name := strings.Trim(origname, ".")
 	switch query.Question[0].Qtype {
 	case dns.TypeA:
-        fmt.Printf("Serving %v\n", name)
-        ips, err := net.DefaultResolver.LookupIPAddr(context.Background(), name)
+        var target string
+
+        for _, m := range r.mappings {
+            if glob.Glob(m.pattern, name) {
+                target = m.target
+                break
+            }
+        }
+
+        fmt.Printf("Serving %v=%v\n", name, target)
+        ips, err := net.DefaultResolver.LookupIPAddr(context.Background(), target)
 
         if err == nil {
+            fmt.Printf("%v=%v is %v\n", name, target, ips[0].IP)
             resp = new(dns.Msg)
-            fmt.Printf("Success %v\n", name)
             rr := &dns.A{
-                Hdr: dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+                Hdr: dns.RR_Header{Name: origname, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
                 A:   ips[0].IP,
             }
             resp.SetReply(query)
